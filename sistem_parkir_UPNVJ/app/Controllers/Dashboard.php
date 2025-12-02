883cbfeb6823360970b8cd1dd4f93ca4aa38d6bd
Ravitirta <ravitirta13207@gmail.com>
2025-12-02 17:30:35 +0700

    pembuatan tampilan awal

---
+++ sistem_parkir_UPNVJ/app/Controllers/Dashboard.php
@@ -0,0 +1,27 @@
+<?php namespace App\Controllers;
+
+use App\Models\AreaModel;
+use App\Models\KendaraanModel;
+
+class Dashboard extends BaseController
+{
+    public function index()
+    {
+        // 1. Panggil Model
+        $areaModel = new AreaModel();
+        $kendaraanModel = new KendaraanModel();
+
+        // 2. Siapkan Data
+        $data = [
+            'title'     => 'Transaksi Masuk',
+            'isi'       => 'dashboard/transaksi_masuk', // View konten
+            'area'      => $areaModel->findAll(),       // Data untuk dropdown Area
+            'kendaraan' => $kendaraanModel->findAll()   // Data untuk dropdown Jenis
+        ];
+
+        // 3. Tampilkan Wrapper
+        return view('layout/wrapper', $data);
+    }
+    
+    // Nanti tambahkan function simpanTransaksi() disini
+}
